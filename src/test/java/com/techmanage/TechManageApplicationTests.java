e98ab7d81b639fa82cdcc91b5d918c0717fd46bc
Fernandak <f-fernanda@outlook.com>
2025-05-09 12:35:24 -0300

    inicializando projeto Spring Boot com dependências web, JPA, H2 e validation

---
+++ src/test/java/com/techmanage/TechManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
